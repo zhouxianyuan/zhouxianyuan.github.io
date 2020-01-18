c00b4532026e39069af8f2e5e358405f54cb7dbd
zhouxianyuan <zxy965211@163.com>
2020-01-18 11:55:09 +0800

    Site updated: 2020-01-18 11:55:09

--- dist/music.js
+++ dist/music.js
@@ -1,7 +1,7 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: false,
+    autoplay: true,
     order: 'list',
     theme: '#FADFA3',
     loop: 'all',
@@ -23,6 +23,12 @@ const ap = new APlayer({
         artist: 'DAOKO / 米津玄師',
         url: 'http://music.163.com/song/media/outer/url?id=ID496869422.mp3',
         cover: 'http://p1.music.126.net/ZUCE_1Tl_hkbtamKmSNXEg==/109951163009282836.jpg',
+      },
+      {
+        name: "Shelter",
+        artist: 'Porter Robinson、Madeon',
+        url: 'http://music.163.com/song/media/outer/url?id=ID425280053.mp3',
+        cover: 'http://p2.music.126.net/9krlgGqhzoAHfK5NIWTZbw==/109951163345022045.jpg',
       }
     ]
 });
