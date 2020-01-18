373affc217fff20b460e77c63ff781a00ddddb31
zhouxianyuan <zxy965211@163.com>
2020-01-18 12:38:46 +0800

    Site updated: 2020-01-18 12:38:46

--- dist/music.js
+++ dist/music.js
@@ -7,19 +7,19 @@ const ap = new APlayer({
       {
         name: "アイロニ",
         artist: '许まじ娘',
-        url: 'http://music.163.com/song/media/outer/url?id=ID31421442.mp3',
+        url: 'https://music.163.com/song?id=31421442',
         cover: 'http://p1.music.126.net/4Zpn57gnArtV3F5xiNBK0g==/109951163598414321.jpg',
       },
       {
         name: "打上花火",
         artist: 'DAOKO / 米津玄師',
-        url: 'http://music.163.com/song/media/outer/url?id=ID496869422.mp3',
+        url: 'https://music.163.com/song?id=496869422',
         cover: 'http://p1.music.126.net/ZUCE_1Tl_hkbtamKmSNXEg==/109951163009282836.jpg',
       },
       {
         name: "Shelter",
         artist: 'Porter Robinson、Madeon',
-        url: 'http://music.163.com/song/media/outer/url?id=ID425280053.mp3',
+        url: 'https://music.163.com/song?id=425280053',
         cover: 'http://p2.music.126.net/9krlgGqhzoAHfK5NIWTZbw==/109951163345022045.jpg',
       }
     ]
