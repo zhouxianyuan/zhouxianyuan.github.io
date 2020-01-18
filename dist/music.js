3ed5ebd03fa4e1d03cdc034c7be70c7c0537a512
zhouxianyuan <zxy965211@163.com>
2020-01-18 12:32:21 +0800

    Site updated: 2020-01-18 12:32:21

--- dist/music.js
+++ dist/music.js
@@ -2,15 +2,7 @@ const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
     autoplay: false,
-    order: 'list',
-    theme: '#FADFA3',
-    loop: 'all',
-    order: 'list',
-    preload: 'auto',
-    //volume: 0.7,
-    mutex: true,
-    listFolded: false,
-    listMaxHeight: 90,
+
     audio: [
       {
         name: "アイロニ",
@@ -18,7 +10,7 @@ const ap = new APlayer({
         url: 'http://music.163.com/song/media/outer/url?id=ID31421442.mp3',
         cover: 'http://p1.music.126.net/4Zpn57gnArtV3F5xiNBK0g==/109951163598414321.jpg',
       },
-      /*{
+      {
         name: "打上花火",
         artist: 'DAOKO / 米津玄師',
         url: 'http://music.163.com/song/media/outer/url?id=ID496869422.mp3',
@@ -29,6 +21,6 @@ const ap = new APlayer({
         artist: 'Porter Robinson、Madeon',
         url: 'http://music.163.com/song/media/outer/url?id=ID425280053.mp3',
         cover: 'http://p2.music.126.net/9krlgGqhzoAHfK5NIWTZbw==/109951163345022045.jpg',
-      }*/
+      }
     ]
 });
