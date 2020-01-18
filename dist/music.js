37d8d7b0aa8f285e0a94d6c4789df8d5804b84c4
zhouxianyuan <zxy965211@163.com>
2020-01-18 12:48:23 +0800

    Site updated: 2020-01-18 12:48:23

--- dist/music.js
+++ dist/music.js
@@ -7,19 +7,19 @@ const ap = new APlayer({
       {
         name: "アイロニ",
         artist: '许まじ娘',
-        url: 'https://music.163.com/song?id=31421442',
+        url: '  http://antiserver.kuwo.cn/anti.s?format=mp3|aac&rid=20673606&type=convert_url&response=res',
         cover: 'http://p1.music.126.net/4Zpn57gnArtV3F5xiNBK0g==/109951163598414321.jpg',
       },
       {
         name: "打上花火",
         artist: 'DAOKO / 米津玄師',
-        url: 'https://music.163.com/song?id=496869422',
+        url: '  http://antiserver.kuwo.cn/anti.s?format=mp3|aac&rid=40174955&type=convert_url&response=res',
         cover: 'http://p1.music.126.net/ZUCE_1Tl_hkbtamKmSNXEg==/109951163009282836.jpg',
       },
       {
         name: "Shelter",
         artist: 'Porter Robinson、Madeon',
-        url: 'https://music.163.com/song?id=425280053',
+        url: '  http://antiserver.kuwo.cn/anti.s?format=mp3|aac&rid=9936460&type=convert_url&response=res',
         cover: 'http://p2.music.126.net/9krlgGqhzoAHfK5NIWTZbw==/109951163345022045.jpg',
       }
     ]
