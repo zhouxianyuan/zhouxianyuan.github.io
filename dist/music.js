11c67d87d81dfa10bf22d0ffa73aba54a67548bc
zhouxianyuan <zxy965211@163.com>
2020-01-30 23:53:32 +0800

    Site updated: 2020-01-30 23:53:31

--- dist/music.js
+++ dist/music.js
@@ -4,22 +4,29 @@ const ap = new APlayer({
     autoplay: false,
 
     audio: [
+      
+      {
+        name: "写给我第一个喜欢的女孩的歌",
+        artist: '西瓜Kune',
+        url: '  https://music.163.com/song/media/outer/url?id=408250378.mp3',
+        cover: 'http://p1.music.126.net/hdrjFYPZXd2HgL-3OMlDIw==/18290375928279193.jpg',
+      },
       {
         name: "アイロニ",
-        artist: '许まじ娘',
-        url: '  http://antiserver.kuwo.cn/anti.s?format=mp3|aac&rid=20673606&type=convert_url&response=res',
+        artist: 'majiko',
+        url: '  https://music.163.com/song/media/outer/url?id=31421442.mp3',
         cover: 'http://p1.music.126.net/4Zpn57gnArtV3F5xiNBK0g==/109951163598414321.jpg',
       },
       {
         name: "打上花火",
         artist: 'DAOKO / 米津玄師',
-        url: '  http://antiserver.kuwo.cn/anti.s?format=mp3|aac&rid=40174955&type=convert_url&response=res',
+        url: '  https://music.163.com/song/media/outer/url?id=496869422.mp3',
         cover: 'http://p1.music.126.net/ZUCE_1Tl_hkbtamKmSNXEg==/109951163009282836.jpg',
       },
       {
         name: "Shelter",
         artist: 'Porter Robinson、Madeon',
-        url: '  http://antiserver.kuwo.cn/anti.s?format=mp3|aac&rid=9936460&type=convert_url&response=res',
+        url: '  https://music.163.com/song/media/outer/url?id=425280053.mp3',
         cover: 'http://p2.music.126.net/9krlgGqhzoAHfK5NIWTZbw==/109951163345022045.jpg',
       }
     ]
